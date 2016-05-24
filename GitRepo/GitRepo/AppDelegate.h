373c26e8503f590811d6954f0ee33d990addf2a0
wellingtonmb88 <Wellington@WELLINGTONs-MacBook-Pro.local>
2016-05-24 19:18:15 -0300

    Added CocoaPods

---
+++ GitRepo/GitRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitRepo
+//
+//  Created by WELLINGTON BARBOSA on 5/24/16.
+//  Copyright © 2016 WELLINGTON BARBOSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
