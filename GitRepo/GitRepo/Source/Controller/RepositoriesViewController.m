ef226947cb78558e05356ce188dbe445594ccd35
wellingtonmb88 <Wellington@WELLINGTONs-MacBook-Pro.local>
2016-05-26 00:30:27 -0300

    Added Infinite Scroll feature to Repositories TableView

--- GitRepo/GitRepo/Source/Controller/RepositoriesViewController.m
+++ GitRepo/GitRepo/Source/Controller/RepositoriesViewController.m
@@ -6,23 +6,26 @@
 //  Copyright © 2016 WELLINGTON BARBOSA. All rights reserved.
 //
 
+#import <UIScrollView+InfiniteScroll.h>
 #import "RepositoriesViewController.h"
 #import "GRGitHubManager.h"
 #import "GRRepositories.h"
 #import "GRRepository.h"
-#import "GROwner.h"
-#import "GRPullRequest.h" 
 #import "GRRepositoryTableViewCell.h"
 #import "MBProgressHUD.h"
 #import "UIAlertController+Utils.h"
 
 @interface RepositoriesViewController ()
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) IBOutlet UISearchController *searchController;
 
 @property (strong, nonatomic) GRGitHubManager *manager;
-@property (strong, nonatomic)GRRepositories * repositories;
-@property (strong, nonatomic)NSArray * pullRequests;
+@property (strong, nonatomic) GRRepositories * repositories;
+@property (strong, nonatomic) NSArray * pullRequests;
+@property (strong, nonatomic) NSString * searchVaue;
+@property (nonatomic) int pageCount;
+
 @end
 
 @implementation RepositoriesViewController
@@ -30,21 +33,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Search controller
-    _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
-    _searchController.delegate = self;
-    _searchController.searchBar.delegate = self;
-    _searchController.searchBar.placeholder = NSLocalizedString(@"alertcontroller.title.searchrepository", @"Search Repository");
-    
-    _searchController.hidesNavigationBarDuringPresentation = false;
-    _searchController.dimsBackgroundDuringPresentation = false;
-    
-    // Add the search bar
-    self.definesPresentationContext = NO;
-    [_searchController.searchBar sizeToFit];
-    
-    self.navigationItem.titleView = _searchController.searchBar;
-    
+    [self setupSearchController];
+    [self setupInfiniteScroll];
 }
 
 #pragma mark - Getters/Setters
@@ -60,17 +50,58 @@
 #pragma mark - UISearchBarDelegate
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     if(searchBar.text != nil && [searchBar.text length] > 0){
-        [self searchRepositoryWithValue: searchBar.text];
+        self.searchVaue = searchBar.text;
+        _pageCount = 1;
+        [self searchRepository];
     }
 }
 
 #pragma mark - Actions
-- (void)searchRepositoryWithValue:(NSString *)value {
+
+- (void) setupSearchController {
+    // Search controller
+    _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    _searchController.delegate = self;
+    _searchController.searchBar.delegate = self;
+    _searchController.searchBar.placeholder = NSLocalizedString(@"alertcontroller.title.searchrepository", @"Search Repository");
+    
+    _searchController.hidesNavigationBarDuringPresentation = false;
+    _searchController.dimsBackgroundDuringPresentation = false;
+    
+    // Add the search bar
+    self.definesPresentationContext = NO;
+    [_searchController.searchBar sizeToFit];
+    
+    self.navigationItem.titleView = _searchController.searchBar;
+}
+
+- (void) setupInfiniteScroll {
+    
+    // change indicator view style to white
+    self.tableView.infiniteScrollIndicatorStyle = UIActivityIndicatorViewStyleGray;
+    
+    typeof(self) __weak __block weakSelf = self;
+    // setup infinite scroll
+    [self.tableView addInfiniteScrollWithHandler:^(UITableView* tableView) {
+        
+        typeof(weakSelf) __strong strongSelf = weakSelf;
+        if (!strongSelf) {
+            return;
+        }
+        _pageCount+=1;
+        [strongSelf searchRepository];
+        
+        // finish infinite scroll animation
+        [tableView finishInfiniteScroll];
+    }];
+}
+
+- (void)searchRepository{
     [self showProgress];
     
     typeof(self) __weak __block weakSelf = self;
     
-    [self.manager searchRepositoriesWith:value atPage:1 withCompletionBlock:^(GRRepositories * repositories, BOOL success) {
+    [self.manager searchRepositoriesWith:self.searchVaue atPage:_pageCount withCompletionBlock:^(GRRepositories * repositories, BOOL success) {
         
         typeof(weakSelf) __strong strongSelf = weakSelf;
         if (!strongSelf) {
